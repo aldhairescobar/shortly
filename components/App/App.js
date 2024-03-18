589d89ae47d75176dd3eccbd591963c7b79010ef
aldhairescobar <aldhair.escobar@gmail.com>
2024-03-17 23:36:20 -0600

    setting the project

---
+++ components/App/App.js
@@ -0,0 +1,146 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <SearchBar />
+      <Features />
+      <CallToAction />
+      <Footer />
+    </>
+  );
+}
+
+function Header() {
+  return (
+    <p>
+      Features
+      <br />
+      Pricing
+      <br />
+      Resources
+      <br />
+      <br />
+      Login
+      <br />
+      Sign Up
+      <br />
+    </p>
+  );
+}
+
+function Hero() {
+  return (
+    <p>
+      More than just shorter links
+      <br />
+      <br />
+      Build your brand’s recognition and get detailed insights <br />
+      on how your links are performing.
+      <br />
+      <br />
+      Get Started
+    </p>
+  );
+}
+
+function SearchBar() {
+  return (
+    <p>
+      Shorten a link here...
+      <br />
+      <br />
+      Shorten It!
+    </p>
+  );
+}
+
+function Features() {
+  return (
+    <p>
+      Advanced Statistics
+      <br />
+      <br />
+      Track how your links are performing across the web with our
+      <br />
+      advanced statistics dashboard.
+      <br />
+      <br />
+      Brand Recognition
+      <br />
+      <br />
+      Boost your brand recognition with each click. Generic links don’t
+      <br />
+      mean a thing. Branded links help instil confidence in your content.
+      <br />
+      <br />
+      Detailed Records
+      <br />
+      <br />
+      Gain insights into who is clicking your links. Knowing when and where
+      <br />
+      people engage with your content helps inform better decisions.
+      <br />
+      <br />
+      Fully Customizable
+      <br />
+      <br />
+      Improve brand awareness and content discoverability through customizable
+      <br />
+      links, supercharging audience engagement.
+    </p>
+  );
+}
+
+function CallToAction() {
+  return (
+    <p>
+      Boost your links today
+      <br />
+      <br />
+      Get Started
+    </p>
+  );
+}
+
+function Footer() {
+  return (
+    <p>
+      Features
+      <br />
+      <br />
+      Link Shortening
+      <br />
+      Branded Links
+      <br />
+      Analytics
+      <br />
+      <br />
+      Resources
+      <br />
+      <br />
+      Blog
+      <br />
+      Developers
+      <br />
+      Support
+      <br />
+      <br />
+      Company
+      <br />
+      <br />
+      About
+      <br />
+      Our Team
+      <br />
+      Careers
+      <br />
+      Contact
+      <br />
+    </p>
+  );
+}
+
+export default App;
